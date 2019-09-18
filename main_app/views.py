2ff99cc686a66d4ff2ba5903cf4b8f5c7fe03367
ManliestBen <benjamintmanley@gmail.com>
2019-09-18 17:01:35 -0500

    add confetti

--- main_app/views.py
+++ main_app/views.py
@@ -120,23 +120,16 @@ def run_sms(request):
   account_sid = 'AC9f4a82594a7168acc47b2506ccc927d1'
   auth_token = '13820f6ceac5c85c875218a33c0e76db'
   client = Client(account_sid, auth_token)
-
-# phone = input("Please enter your phone number with area code using no spaces: ")
-# phoneadj = '+1' + phone
-# print (phoneadj)
-# message = input("Please enter the message you'd like to send:")
-# print (message)
-
-
-
+  # phone = input("Please enter your phone number with area code using no spaces: ")
+  # phoneadj = '+1' + phone
+  # print (phoneadj)
+  # message = input("Please enter the message you'd like to send:")
+  # print (message)
   message = client.messages \
     .create(
          body= 'OMFG IT WORKED!',
          from_='+18705222095',
          to= '+17853411918'
      )
-
   print(message.sid)
-  
   return redirect('success')
-  # return HttpResponse('')
