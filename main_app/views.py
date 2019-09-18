3ec89ebecbad66b2b16e68b2bbcd944a0adb20f4
ManliestBen <benjamintmanley@gmail.com>
2019-09-18 17:20:05 -0500

    add SMS notification based on user.userprofile.phone

--- main_app/views.py
+++ main_app/views.py
@@ -117,19 +117,23 @@ def assoc_user(request, event_id, user_id):
 #         return http.HttpResponseForbidden("Cannot delete other's posts")
 
 def run_sms(request):
+  
   account_sid = 'AC9f4a82594a7168acc47b2506ccc927d1'
   auth_token = '13820f6ceac5c85c875218a33c0e76db'
   client = Client(account_sid, auth_token)
+  phoneadj = "+1" + request.user.userprofile.phone
+  # print( "+1" + request.user.userprofile.phone )
   # phone = input("Please enter your phone number with area code using no spaces: ")
   # phoneadj = '+1' + phone
   # print (phoneadj)
   # message = input("Please enter the message you'd like to send:")
   # print (message)
+  
   message = client.messages \
     .create(
          body= 'OMFG IT WORKED!',
          from_='+18705222095',
-         to= '+17853411918'
+         to= phoneadj
      )
   print(message.sid)
   return redirect('success')
